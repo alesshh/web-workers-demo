0cc329776d050d24a093af8b7575e03cebabfbc5
Alessandro Tegner <alessandro.tegner@gmail.com>
2016-08-15 21:49:48 -0300

    Resolve the challenge

--- scripts/image-app.js
+++ scripts/image-app.js
@@ -5,6 +5,7 @@
   imageLoader.addEventListener('change', handleImage, false);
   var canvas = document.querySelector('#image');
   var ctx = canvas.getContext('2d');
+  var worker = new Worker('scripts/worker.js');
 
   function handleImage(e){
     var reader = new FileReader();
@@ -35,28 +36,19 @@
   }
 
   function manipulateImage(type) {
-    var a, b, g, i, imageData, j, length, pixel, r, ref;
-    imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    var imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
 
     toggleButtonsAbledness();
 
     // Hint! This is where you should post messages to the web worker and
     // receive messages from the web worker.
 
-    length = imageData.data.length / 4;
-    for (i = j = 0, ref = length; 0 <= ref ? j <= ref : j >= ref; i = 0 <= ref ? ++j : --j) {
-      r = imageData.data[i * 4 + 0];
-      g = imageData.data[i * 4 + 1];
-      b = imageData.data[i * 4 + 2];
-      a = imageData.data[i * 4 + 3];
-      pixel = manipulate(type, r, g, b, a);
-      imageData.data[i * 4 + 0] = pixel[0];
-      imageData.data[i * 4 + 1] = pixel[1];
-      imageData.data[i * 4 + 2] = pixel[2];
-      imageData.data[i * 4 + 3] = pixel[3];
-    }
-    toggleButtonsAbledness();
-    return ctx.putImageData(imageData, 0, 0);
+    worker.postMessage({imageData: imageData, type: type});
+
+    worker.onmessage = function(e) {
+      toggleButtonsAbledness();
+      return ctx.putImageData(e.data, 0, 0);
+    };
   };
 
   function revertImage() {
